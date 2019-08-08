bf5533a2edbf2bc09c42b90ccac0fbc49f730f97
rafapereirabr <rafa.pereira.br@gmail.com>
2019-08-08 15:22:48 -0300

    script to calculate access using data.table

--- scripts/02-calculate-accessibility_dt.R
+++ scripts/02-calculate-accessibility_dt.R
@@ -1,83 +1,99 @@
-# List all .csv outputs of OTP
-  tt_before <- 
-    list.files(here("data"), full.names = TRUE)[grep("tt_before*", 
-                                               list.files(here("data")))]
-  tt_after <- 
-    list.files(here("data"), full.names = TRUE)[grep("tt_after*", 
+
+
+# Read travel time matrixes into memory
+
+  # List all .csv outputs of OTP
+    tt_before <- list.files(here("data"), full.names = TRUE)[grep("tt_before*", 
+                                                 list.files(here("data")))]
+    
+    tt_after <- list.files(here("data"), full.names = TRUE)[grep("tt_after*", 
                                              list.files(here("data")))]
 
+  # Read travel time matrixes
+    skims_before <- lapply(X=tt_before, FUN=fread) %>% rbindlist()
+    skims_after <- lapply(X=tt_after, FUN=fread) %>% rbindlist()
+
+  # pile travel time matrices up
+    skims_after$when <- 'after'
+    skims_before$when <- 'before'
+    ttmatrix <- rbindlist(list(skims_after, skims_before))
+    head(ttmatrix)
+    
+    # clean memory
+      rm(skims_after, skims_before)
+      gc(reset = T)
+
+      
+# Read jobs data and drop geometry column
+  hex_lodes <- readRDS('./data/hex_lodes.rds') %>% setDT()
+  hex_lodes$geometry <- NULL 
+  head(hex_lodes)
   
   
-# Read travel time matrixes into memory
-  skims_before <- lapply(X =tt_before, FUN=fread) %>% rbindlist()
-  skims_after <- lapply(X =tt_after, FUN=fread) %>% rbindlist()
-
+# Read census data and drop geometry column
+  hex_trandep <- readRDS('./data/hex_trandep.rds') %>% setDT()
+  hex_trandep$geometry <- NULL 
+  head(hex_trandep)
+  
+  # Reshape data from long to wide format   
+  hex_trandep_wide <- data.table::dcast(hex_trandep, hexid ~ variable, value.var = c("est"))
+  head(hex_trandep_wide)
   
   
+
+# Merge job count with OD Matrix, allocating job counts to Destination  
   
-# TODO: Calculate accessibility for each OD pair at each time slice and 
-# place it in a new table 
+  # origin
+    ttmatrix <- ttmatrix[hex_trandep_wide, on=c('origin'='hexid'), nomatch=0]
+  # destination
+    ttmatrix <- ttmatrix[hex_lodes, on=c('origin'='hexid'), nomatch=0]
+  
+  head(ttmatrix)
+  gc(reset = T)
+  
+  
+  # ttmatrix <- copy(skims_before)
+    
+  
+  # Calculate accessibility for each OD pair at each time slice and when (before/after)
+    access_intraday <- setDT(ttmatrix)[travel_time < 3600,
+                                            .( acc30 = sum( totjobs[which( travel_time <= 30)], na.rm=T)
+                                             , acc60 = sum( totjobs[which( travel_time <= 60)], na.rm=T)
+                                             , acc90 = sum( totjobs[which( travel_time <= 90)], na.rm=T)),
+                                         by=.(origin, depart_time, when) ]
+    
+  # Calculate Median accessibility before/after so that infrequent service doesn't appear attractive
+    access <- access_intraday[, .( acc30 = median( acc30, na.rm=T)
+                                 , acc60 = median( acc60, na.rm=T)
+                                 , acc90 = median( acc90, na.rm=T)), 
+                              by=.(origin, when) ]
+    
 
-# Write jobs to database
-dbWriteTable(con, "jobs", st_drop_geometry(hex_lodes))
+    
+# Combine before and after into a single dataframe for plotting [LONG Format]
+  # add origin land use info
+    output_long <- left_join(access, hex_trandep_wide, by=c('origin'='hexid'))
+    # add geometry
+    harris_hex <- readRDS("./data/harris_hex.rds") # Load hex grid
+    output_long <- left_join(output_long, harris_hex, by=c('origin'='hexid')) %>% st_as_sf()
+    head(output_long)
+    
 
-# Create view that joins the jobs and skims for the two time periods
-dbExecute(con,
-  "CREATE VIEW time_jobs_before AS
-  SELECT origin, destination, totjobs, travel_time
-  FROM skims_before INNER JOIN jobs
-  ON skims_before.destination = jobs.hexid
-  WHERE skims_before.travel_time < 3600")
+# Combine before and after [WIDE Format]
+    # Reshape data from wide to long format   
+    output_wide <- data.table::dcast(access, origin ~ when, value.var = c("acc30", "acc60", "acc90"))
+    # add geometry
+    output_wide <- left_join(output_wide, harris_hex, by=c('origin'='hexid')) %>% st_as_sf()
+    head(output_wide)
 
-# dbExecute(con, "DROP VIEW time_jobs_before")
-
-dbExecute(con,
-  "CREATE VIEW time_jobs_after AS
-  SELECT origin, destination, totjobs, travel_time
-  FROM skims_after INNER JOIN jobs
-  ON skims_after.destination = jobs.hexid
-  WHERE skims_after.travel_time < 3600")
-
-# dbExecute(con, "DROP VIEW time_jobs_after")
-
-# Once this is ready, then just grab each of the threshold accessibilites 
-# Manually calculate the mean so that infrequent service doesn't appear attractive
-acc60_before <- dbGetQuery(con, 
-  "SELECT origin AS hexid, sum(totjobs) / 8 AS acc60
-  FROM time_jobs_before
-  GROUP BY origin")          
-
-acc60_before_hex <- left_join(harris_hex, acc60_before)
-
-acc60_after <- dbGetQuery(con, 
-  "SELECT origin AS hexid, sum(totjobs) / 8 AS acc60 
-  FROM time_jobs_after
-  GROUP BY origin")          
-
-acc60_after_hex <- left_join(harris_hex, acc60_after)
-
-# Combine before and after into a single dataframe for plotting
-acc60 <- rbind(mutate(acc60_before_hex, when = "before"),
-               mutate(acc60_after_hex, when = "after"))
-
-acc60[is.na(acc60$acc60), ]$acc60 <- 0
-
-# Combine before and after into a single dataframe for comparisons
-acc60_comp <- inner_join(rename(acc60_before, acc60_b = acc60),
-                         rename(acc60_after, acc60_a = acc60))
-
-dbDisconnect(con, shutdown = TRUE)
-
-# Troubleshooting --------------------------------------------------------------
-
-before <- dbGetQuery(con, "SELECT * FROM time_jobs_before")
-after <- dbGetQuery(con, "SELECT * FROM time_jobs_after")
-
-# Setting the threshold within the OTP process means that some number of longer
-# trips are not skimmed, so when average travel times are calculated they 
-# would be lower than they would be otherwise. 
-# Would this be mitigated by calculating accessibility at each time slice and
-# aggregating? 
-
-dim(before)
-dim(after)
+    
+# Save accessibility outputs    
+    saveRDS(output_wide, './data/output_wide.rds', compress=T)
+    saveRDS(output_long, './data/output_long.rds', compress=T)
+    
+    
+    
+    
+    
+    
+    
